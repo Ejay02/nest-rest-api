7e858b024f424daf9107a3cf7a4e43948cf91925
Ejay02 <e.jae02@gmail.com>
2022-04-12 10:09:30 +0100

    connect to db update service and controller

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,41 +1,42 @@
 import {
-  Body,
   Controller,
-  Delete,
   Get,
-  Param,
   Post,
   Put,
+  Delete,
+  Body,
+  Param,
 } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
+import { ItemsService } from './items.service';
+import { Item } from './interfaces/item.interface';
 
 @Controller('items')
 export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
+
   @Get()
-  findAll(): string {
-    return 'This action returns all items.';
+  findAll(): Promise<Item[]> {
+    return this.itemsService.findAll();
   }
 
-  @Get('/:id')
-  findOne(@Param('id') id: string): string {
-    return `This action returns a #${id} item.`;
+  @Get(':id')
+  findOne(@Param('id') id): Promise<Item> {
+    return this.itemsService.findOne(id);
   }
 
   @Post()
-  create(@Body() createItemDto: CreateItemDto): string {
-    // return 'This action adds an item.';
-    return `Name: ${createItemDto.name} 
-    Description: ${createItemDto.description} 
-    Qty: ${createItemDto.qty}`;
+  create(@Body() createItemDto: CreateItemDto): Promise<Item> {
+    return this.itemsService.create(createItemDto);
   }
 
-  @Put('/:id')
-  update(@Body() updateItemDto: CreateItemDto, @Param('id') id): string {
-    return `This action updates item #${id} - Name: ${updateItemDto.name} `;
+  @Delete(':id')
+  delete(@Param('id') id): Promise<Item> {
+    return this.itemsService.delete(id);
   }
 
-  @Delete('/:id')
-  delete(@Param('id') id): string {
-    return `This action removes item #${id}.`;
+  @Put(':id')
+  update(@Body() updateItemDto: CreateItemDto, @Param('id') id): Promise<Item> {
+    return this.itemsService.update(id, updateItemDto);
   }
 }
